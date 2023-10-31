160345c9b870b57687f0e2031af10c75a6139592
Khairull Anuar Jamlus <anuar.jamlus@gmail.com>
2023-10-31 19:28:58 +0800

    Add to test updateAllStatusesInChunks

--- tests/Unit/TodoServiceTest.php
+++ tests/Unit/TodoServiceTest.php
@@ -2,6 +2,7 @@
 
 namespace Tests\Unit;
 
+use App\Jobs\UpdateTodoStatusJob;
 use App\Models\Todo;
 use App\Services\TodoService;
 use Illuminate\Foundation\Testing\RefreshDatabase;
@@ -38,4 +39,30 @@ class TodoServiceTest extends TestCase
 
         $this->assertEquals('done', $todo->fresh()->status);
     }
+
+    public function test_it_can_update_a_todo_status_to_pending(): void
+    {
+        $todo = Todo::factory()->create(['status' => 'done']);
+        $this->todoService->updateStatus($todo->id, 'pending');
+
+        $this->assertEquals('pending', $todo->fresh()->status);
+    }
+
+    public function test_it_can_update_status_of_all_todos(): void
+    {
+        Todo::factory()->count(50)->create(['status' => 'pending']);
+        Todo::factory()->count(50)->create(['status' => 'done']);
+
+        $this->assertDatabaseCount('todos', 100);
+        $this->assertCount(50, Todo::where('status', 'pending')->get());
+
+        $todoService = new TodoService();
+        $todoService->updateAllStatusesInChunks('done');
+        $this->assertCount(0, Todo::where('status', 'pending')->get());
+        $this->assertCount(100, Todo::where('status', 'done')->get());
+
+        $todoService->updateAllStatusesInChunks('pending');
+        $this->assertCount(100, Todo::where('status', 'pending')->get());
+        $this->assertCount(0, Todo::where('status', 'done')->get());
+    }   
 }
