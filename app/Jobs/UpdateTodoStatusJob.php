122c04b989faddfcdd9d1bff0ee3a35974812f17
Khairull Anuar Jamlus <anuar.jamlus@gmail.com>
2023-10-31 19:28:58 +0800

    Add logging to job for debugging

--- app/Jobs/UpdateTodoStatusJob.php
+++ app/Jobs/UpdateTodoStatusJob.php
@@ -9,6 +9,7 @@ use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Foundation\Bus\Dispatchable;
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Queue\SerializesModels;
+use Illuminate\Support\Facades\Log;
 
 class UpdateTodoStatusJob implements ShouldQueue
 {
@@ -18,11 +19,13 @@ class UpdateTodoStatusJob implements ShouldQueue
 
     public function __construct(string $status)
     {
+        Log::info('Initialising to do status updates.');
         $this->status = $status;
     }
 
     public function handle(TodoService $todoService): void
     {
         $todoService->updateAllStatusesInChunks($this->status);
+        Log::info('Status update completed.');
     }
 }
