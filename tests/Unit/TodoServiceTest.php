8072e27886c0aaaf9c4151a351176034a38d8e5b
Khairull Anuar Jamlus <anuar.jamlus@gmail.com>
2023-10-31 19:28:58 +0800

    Add test to retrieve paginated list of Todo

---
+++ tests/Unit/TodoServiceTest.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Tests\Unit;
+
+use App\Models\Todo;
+use App\Services\TodoService;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class TodoServiceTest extends TestCase
+{
+    use RefreshDatabase, WithFaker;
+
+    protected $todoService;
+
+    public function setUp(): void
+    {
+        parent::setUp();
+        $this->todoService = app(TodoService::class);
+    }
+
+
+    public function test_it_can_fetch_paginated_todos(): void
+    {
+        Todo::factory()->count(50)->create();
+
+        $todos = $this->todoService->getAllPaginated(10);
+
+        $this->assertCount(10, $todos->items());
+        $this->assertEquals(50, $todos->total());
+    }
+}
