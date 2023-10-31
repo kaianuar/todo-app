ca1513d94bd0de5ecdcf2798198e883d890bcc75
Khairull Anuar Jamlus <anuar.jamlus@gmail.com>
2023-10-31 19:28:58 +0800

    Add test for Todo patch endpoint

--- tests/Feature/TodoApiTest.php
+++ tests/Feature/TodoApiTest.php
@@ -50,4 +50,58 @@ class TodoApiTest extends TestCase
                 ->assertJsonCount(10, 'data')
                 ->assertJsonPath('meta.total', 50);
     }
+
+    /**
+     * Test updating the status of a Todo by ID.
+     */
+    public function test_update_status()
+    {
+        $todo = Todo::factory()->create();
+        $response = $this->patchJson("/api/v1/todos/{$todo->id}", [
+            'status' => 'done'
+        ]);
+
+        $response->assertStatus(200)
+                ->assertJsonStructure([
+                    'data' => [
+                        'id',
+                        'title',
+                        'description',
+                        'status'
+                    ]
+                ])
+                ->assertJsonPath('data.status', 'done');
+
+        $this->assertDatabaseHas('todos', [
+            'id' => $todo->id,
+            'status' => 'done'
+        ]);
+    }
+
+    /**
+     * Test updating the status of a Todo that doesn't exist.
+     */
+    public function test_update_status_404()
+    {
+        $response = $this->patchJson("/api/v1/todos/9999999", [
+            'status' => 'done'
+        ]);
+
+        $response->assertStatus(404)
+                ->assertJson(['error' => 'Todo not found']);
+    }
+
+    /**
+ * Test updating the status of a Todo with an invalid request.
+ */
+public function test_update_status_422()
+{
+    $todo = Todo::factory()->create();
+
+    // Status is missing
+    $response = $this->patchJson("/api/v1/todos/{$todo->id}", []);
+
+    $response->assertStatus(422)
+            ->assertJsonValidationErrors(['status']);
+}
 }
